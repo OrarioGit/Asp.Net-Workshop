d8f28552e9de50c23558f12d9a4a9d5735665713
Orario <1104137112@gm.kuas.edu.tw>
2018-03-29 16:52:41 +0800

    add Model Employees

---
+++ Asp_Net Project/Models/Employees.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Asp_Net_Project.Models
+{
+    public class Employees
+    {
+        public int EmployeeID { get; set;}
+
+        public string LastName { get; set;}
+
+        public string FirstName { get; set;}
+    }
+}
