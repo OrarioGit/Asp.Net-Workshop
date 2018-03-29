54f4c5c1be3330bac6511989a7825f4e51f1feb7
Orario <1104137112@gm.kuas.edu.tw>
2018-03-29 16:54:23 +0800

    add Model Shippers

---
+++ Asp_Net Project/Models/Shippers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Asp_Net_Project.Models
+{
+    public class Shippers
+    {
+        public int ShipperID { get; set;}
+
+        public string CompanyName { get; set;}
+
+        public string Phone { get; set;}
+    }
+}
