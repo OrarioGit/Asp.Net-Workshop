c8fdb73836a671fedeaa4c1e7462f924ea058469
Orario <1104137112@gm.kuas.edu.tw>
2018-03-29 16:48:28 +0800

    add Model Orders

---
+++ Asp_Net Project/Models/Orders.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Asp_Net_Project.Models
+{
+    public class Orders
+    {
+        public int OrderID { get; set;}
+
+        public int CustomerID { get; set;}
+
+        public int EmployeeID { get; set;}
+
+        public DateTime OrderDate { get; set;}
+
+        public DateTime RequiredDate { get; set;}
+
+        public DateTime ShippedDate { get; set;}
+
+        public int ShipperID { get; set;}
+
+        public decimal Freight { get; set;}
+
+        public string ShipAddress { get; set;}
+
+        public string ShipCity { get; set;}
+
+        public string ShipRegion { get; set;}
+
+        public string ShipPostalCode { get; set;}
+
+        public string ShipCountry { get; set;}
+    }
+}
