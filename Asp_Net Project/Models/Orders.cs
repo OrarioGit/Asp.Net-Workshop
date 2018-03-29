db665034095a2bd59d4eb0e164b86b1533a00ed3
Orario <1104137112@gm.kuas.edu.tw>
2018-03-29 17:50:56 +0800

    add View insertOrders

--- Asp_Net Project/Models/Orders.cs
+++ Asp_Net Project/Models/Orders.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
 
 namespace Asp_Net_Project.Models
 {
@@ -13,24 +15,35 @@ namespace Asp_Net_Project.Models
 
         public int EmployeeID { get; set;}
 
+        [DisplayName("訂單日期")]
+        [Required()]
         public DateTime OrderDate { get; set;}
 
+        [DisplayName("需要日期")]
+        [Required()]
         public DateTime RequiredDate { get; set;}
 
+        [DisplayName("出貨日期")]
         public DateTime ShippedDate { get; set;}
 
         public int ShipperID { get; set;}
 
+        [DisplayName("運費")]
         public decimal Freight { get; set;}
 
+        [DisplayName("出貨地址")]
         public string ShipAddress { get; set;}
 
+        [DisplayName("出貨城市")]
         public string ShipCity { get; set;}
 
+        [DisplayName("出貨地區")]
         public string ShipRegion { get; set;}
 
+        [DisplayName("郵遞區號")]
         public string ShipPostalCode { get; set;}
 
+        [DisplayName("出貨國家")]
         public string ShipCountry { get; set;}
     }
 }
